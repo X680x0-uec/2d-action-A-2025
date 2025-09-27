0ec6e1c2b56741f855340e35c11534ec37abb4f4
Neko-Rintaro <o2511056@gl.cc.uec.ac.jp>
2025-09-27 22:41:59 +0900

    雨リメイク

--- Assets/Scenes/Rintaro/RainManagerScript.cs
+++ Assets/Scenes/Rintaro/RainManagerScript.cs
@@ -6,9 +6,21 @@ using UnityEngine;
 public class RainManagerScript : MonoBehaviour
 {
 
-    [Header("雨の設定(雨は指定したx座標を中心に展開 強さは0~1 幅は0より大きい値)")]
+    [Header("雨の追加＆編集")]
 
     public List<RainVector3> rainParamator = new List<RainVector3>();
+
+    [System.Serializable]
+    public struct RainVector3
+    {
+        public float locate;
+        //雨の場所(左端)
+        public float Strength;
+        //雨の強さ(0~1 0.5から霧が出てくる)
+        public float Width;
+        //雨の広さ(0より大きい値 x座標換算)
+    }
+
     public GameObject Rain;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +28,7 @@ public class RainManagerScript : MonoBehaviour
         int count = rainParamator.Count;
         for (int i = 0; i < count; i++)
         {
-            var r = Instantiate(Rain, new Vector3(rainParamator[i].locate, 0, 0), Quaternion.identity);
+            var r = Instantiate(Rain, new Vector3(rainParamator[i].locate+(rainParamator[i].Width/2), 0, 0), Quaternion.identity);
             var s = r.GetComponent<RainScript2D>();
             s.RainIntensity = rainParamator[i].Strength;
             s.widthLimit = rainParamator[i].Width;
@@ -24,10 +36,4 @@ public class RainManagerScript : MonoBehaviour
     }
 }
 
-[System.Serializable]
-public struct RainVector3
-{
-    public float locate;
-    public float Strength;
-    public float Width;
-}
+
