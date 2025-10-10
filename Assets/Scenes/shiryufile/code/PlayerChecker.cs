f208f427fd400ed899e4435b62b40502dc13ed49
SUeda911 <u2511044@gl.cc.uec.ac.jp>
2025-10-10 18:46:31 +0900

    「PlayerCheckerMaker.cs」をオブジェクトに付けると、敵キャラを自動生成できる。

--- Assets/Scenes/shiryufile/code/PlayerChecker.cs
+++ Assets/Scenes/shiryufile/code/PlayerChecker.cs
@@ -5,21 +5,30 @@ public class PlayerChecker : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] GameObject spawnedBy;
-    GameObject Player = GameObject.FindWithTag("Player");
+    [SerializeField] GameObject Player;
+    PlayerCheckerMaker playerCheckerMaker;
+    float activateDistance;
     
     public void Initialize(GameObject caller)
     {
         spawnedBy = caller;
+        playerCheckerMaker = spawnedBy.GetComponent<PlayerCheckerMaker>();
+        activateDistance = playerCheckerMaker.activateDistance;
+
     }
 
     void Start()
     {
-        
+        Player = GameObject.FindWithTag("walker");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Player.transform.position.x - transform.position.x >= activateDistance)
+        {
+            spawnedBy.gameObject.SetActive(true);
+            Destroy(this.gameObject);
+        }
     }
 }
